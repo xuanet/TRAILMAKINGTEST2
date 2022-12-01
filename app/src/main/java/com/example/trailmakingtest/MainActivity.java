f68232e6e74a739bea2e5453f5657dfb6ca41e7d
xuanet <89868629+xuanet@users.noreply.github.com>
2022-12-01 12:54:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/trailmakingtest/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.trailmakingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.*;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button button2;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btnNextPage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTMT();
+            }
+        });
+    }
+
+    public void openTMT() {
+        Intent intent = new Intent(this, tmt.class);
+        startActivity(intent);
+    }
+
+
+    public void onBtnClick (View view) {
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName = findViewById(R.id.txtLastName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        EditText edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
+        EditText edtTxtLastName = findViewById(R.id.edtTxtLastName);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        txtFirstName.setText("First Name: " + edtTxtFirstName.getText().toString());
+        txtLastName.setText("Last Name: " + edtTxtLastName.getText().toString());
+        txtEmail.setText("Info: " + edtTxtEmail.getText().toString());
+
+        firstName = edtTxtFirstName.getText().toString();
+        lastName = edtTxtLastName.getText().toString();
+        email = edtTxtEmail.getText().toString();
+
+        Log.i("tag", firstName);
+        Log.i("tag", lastName);
+        Log.i("tag", email);
+
+        Display mdisp = getWindowManager().getDefaultDisplay();
+        int maxX = mdisp.getWidth();
+        int maxY = mdisp.getHeight();
+        Log.i("tag", String.valueOf(maxX));
+        Log.i("tag", String.valueOf(maxY));
+
+    }
+}
---
+++ app/src/main/java/com/example/trailmakingtest/tmt.java
@@ -0,0 +1,241 @@
+package com.example.trailmakingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.*;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.*;
+import android.widget.Button;
+import android.widget.EditText;
+import java.lang.Math;
+
+//import static com.example.trailmakingtest.display.colorList;
+//import static com.example.trailmakingtest.display.current_brush;
+//import static com.example.trailmakingtest.display.pathList;
+
+public class tmt extends AppCompatActivity {
+
+    public static long start;
+    public static long end;
+//    private long startTime;
+//    private long endTime;
+
+//    public static Path path = new Path();
+//    public static Paint paint_brush = new Paint();
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_tmt);
+//    }
+//
+//
+//    public void startStopwatch(View view) {
+//        start = SystemClock.elapsedRealtime();
+//    }
+//
+//    public void stopStopwatch(View view) {
+//        end = SystemClock.elapsedRealtime();
+//        int total = (int) (end - start);
+//
+//        EditText edtTime = findViewById(R.id.Time);
+//
+//        edtTime.setText(String.valueOf(total) + " ms");
+//    }
+//}
+
+    DrawingView dv;
+    private Paint mPaint;
+    public long startTime;
+    public long endTime;
+    private Button button;
+
+    public void onStartClick(View view) {
+        startTime = SystemClock.elapsedRealtime();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tmt);
+        dv = new DrawingView(this);
+        setContentView(dv);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(12);
+
+    }
+
+
+//    public void startStopwatch(View view) {
+//        start = SystemClock.elapsedRealtime();
+//    }
+//
+    public void displayTime(View view) {
+        int total = (int) (end - start);
+
+        EditText edtTime = findViewById(R.id.Time);
+
+        edtTime.setText(String.valueOf(total) + " ms");
+    }
+
+
+    public class DrawingView extends View {
+
+        public int width;
+        public  int height;
+        private Bitmap mBitmap;
+        private Canvas  mCanvas;
+        private Path    mPath;
+        private Paint   mBitmapPaint;
+        Context context;
+        private Paint circlePaint;
+        private Path circlePath;
+
+        private float radius;
+        private float[][] circleArray;
+        private int currentIndex;
+
+        public DrawingView(Context c) {
+            super(c);
+            context=c;
+            mPath = new Path();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLUE);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(4f);
+
+            // Specific to this test
+            currentIndex = 0;
+            this.radius = 60;
+            this.circleArray = new float[8][];
+            circleArray[0] = new float[] {521, 266};
+            circleArray[1] = new float[] {86, 340};
+            circleArray[2] = new float[] {265, 78};
+            circleArray[3] = new float[] {730, 328};
+            circleArray[4] = new float[] {578, 528};
+            circleArray[5] = new float[] {196, 654};
+            circleArray[6] = new float[] {712, 782};
+            circleArray[7] = new float[] {66, 904};
+        }
+
+        private boolean success(float xpos, float ypos, int index, float radius) {
+            float circleX = circleArray[index][0];
+            float circleY = circleArray[index][1];
+            float deltax = circleX - xpos;
+            float deltay = circleY - ypos;
+            float distance = (float) Math.pow((Math.pow(deltax,2) + Math.pow(deltay,2)), 0.5);
+            Log.i("tag", String.valueOf(distance));
+
+            if (distance < radius) return true;
+            return false;
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+//            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            Bitmap workingBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.tmt_sample_small);
+            mBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
+            mBitmap = Bitmap.createScaledBitmap(mBitmap, 800, 1050, true);
+            mCanvas = new Canvas(mBitmap);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawPath( mPath,  mPaint);
+            canvas.drawPath( circlePath,  circlePaint);
+        }
+
+        private float mX, mY;
+        private static final float TOUCH_TOLERANCE = 4;
+
+        private void touch_start(float x, float y) {
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+        }
+
+        private void touch_move(float x, float y) {
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y - mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                mX = x;
+                mY = y;
+
+                circlePath.reset();
+                circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
+                Log.i("tag", "XPOS " + String.valueOf(x));
+                Log.i("tag", "YPOS " + String.valueOf(y));
+                Log.i("tag", String.valueOf(currentIndex));
+
+                if (currentIndex == 0) tmt.start = SystemClock.elapsedRealtime();
+
+                if (success(x, y, currentIndex, radius)) {
+                    if (mPaint.getColor() == -16711936 && currentIndex!=0) {
+                        mPaint.setColor(Color.RED);
+                    }
+                    else if (currentIndex!=7) mPaint.setColor(Color.GREEN);
+
+                    if (currentIndex == 7) {
+                        tmt.end = SystemClock.elapsedRealtime();
+                        setContentView(R.layout.activity_tmt);
+                    }
+
+                    if (currentIndex!=circleArray.length-1) {
+                        currentIndex++;
+                    }
+                }
+            }
+        }
+
+        private void touch_up() {
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mPaint);
+            // kill this so we don't double draw
+            mPath.reset();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
+    }
+}
